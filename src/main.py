646e2681d5fcbca226a4b813146c1dca3108bf16
Minhan-Bae <yuyuqw92@gmail.com>
2023-06-07 13:40:55 +0900

    move: move main.py to src

--- src/main.py
+++ src/main.py
@@ -30,7 +30,29 @@ import pandas as pd
 from tqdm import tqdm
 from torch.cuda.amp import autocast, GradScaler
 
-from src.configs import default as C
+from configs import default
+from utils import (
+    arg_parser, seed_utils, trainer
+)
 
 if __name__=='__main__':
-    C
+    # initial step
+    C = default
+    seed_utils.seed_everything(C.SEED)
+
+    # parse argument
+    args = arg_parser.parse_args(C)
+    arg_parser.print_args(args)
+    
+    print(type(args))
+    print(args.criterion)
+    # torch.cuda.set_device(args.device_id[0])
+    
+    early_cnt = 0
+    for epoch in range(1, args.epochs):
+        scaler = GradScaler()
+        
+        # adjust learning rate
+        lr = adjust_learning_rate()
+    
+    
