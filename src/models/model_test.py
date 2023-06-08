e1d870d406b54e27d69c7426f74ff1f766c206cb
Minhan-Bae <yuyuqw92@gmail.com>
2023-06-08 10:42:46 +0900

    feat: add smp model.py

--- src/models/model_test.py
+++ src/models/model_test.py
@@ -1,55 +1,41 @@
-import torch
-from torchvision.models.detection import maskrcnn_resnet50_fpn
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+import torch.nn as nn
+import segmentation_models_pytorch as smp
 
-def get_instance_segmentation_model(num_classes):
-    # Load a model pre-trained on COCO and get the reference to the output layers
-    model = maskrcnn_resnet50_fpn(pretrained=True)
 
-    # Get the number of input features for the classifier
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
+# DeepLabV3
+class DeepLabV3(nn.Module):
+	def __init__(self, num_classes= 7):
+		super(DeepLabV3, self).__init__()
+		self.model = smp.DeepLabV3(
+				encoder_name='efficientnet-b3',
+				encoder_weights='imagenet',
+				in_channels=3, classes=num_classes
+				)
+	def forward(self, x):
+		return self.model(x)
 
-    # Replace the pre-trained head with a new one
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-    # Now get the number of input features for the mask classifier
-    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    hidden_layer = 256
+#DeepLabV3Plus
+class DeepLabV3Plus(nn.Module):
+	def __init__(self, num_classes= 7):
+		super(DeepLabV3Plus, self).__init__()
+		self.model = smp.DeepLabV3Plus(
+				encoder_name='efficientnet-b3',
+				encoder_weights='imagenet',
+				in_channels=3, classes=num_classes
+				)
+	def forward(self, x):
+		return self.model(x)
 
-    # And replace the mask predictor with a new one
-    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
-                                                       hidden_layer,
-                                                       num_classes)
-    return model
 
-def main():
-    # Define the number of classes for your problem
-    num_classes = 2
-
-    # Get the model using our helper function
-    model = get_instance_segmentation_model(num_classes)
-
-    # Move model to the device
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    model.to(device)
-
-    # For Mask R-CNN, we can't print the summary using torchsummary because the input size is variable
-    # Instead, we will try a forward pass with dummy data to make sure the model works
-    x = torch.rand(3, 208, 278).unsqueeze(0).to(device)
-    model.eval()
-    with torch.no_grad():
-        predictions = model(x)
-
-    # Print input and output shapes
-    print(f"Input shape: {x.shape}")
-    for idx, prediction in enumerate(predictions):
-        print(f"\nOutput shape for prediction {idx + 1}:")
-        for key, value in prediction.items():
-            if torch.is_tensor(value):
-                print(f"{key}: {value.shape}")
-            else:
-                print(f"{key}: {value}")
-
-if __name__ == "__main__":
-    main()
+# UnetPlusPlus
+class UnetPlusPlus(nn.Module):
+	def __init__(self, num_classes= 7):
+		super(UnetPlusPlus, self).__init__()
+		self.model = smp.UnetPlusPlus(
+				encoder_name='efficientnet-b3',
+				encoder_weights='imagenet',
+				in_channels=3, classes=num_classes
+				)
+	def forward(self, x):
+		return self.model(x)
