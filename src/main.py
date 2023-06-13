c90332057f283326171ddaeb1ecefb751c5e2102
Minhan-Bae <yuyuqw92@gmail.com>
2023-06-13 16:11:55 +0900

    feat: update main.py

--- src/main.py
+++ src/main.py
@@ -2,57 +2,76 @@
 # -*- coding:utf-8 -*-
 
 import os
-import warnings
-
-warnings.filterwarnings("ignore")
-
 import gc
-
-gc.collect()
-
-import argparse
+import warnings
 import time
 import logging
-from pathlib import Path
-
 import torch
-import torch.backends.cudnn as cudnn
-
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-torch.cuda.empty_cache()
-
-import time
 import numpy as np
-import pandas as pd
 from tqdm import tqdm
 from torch.cuda.amp import autocast, GradScaler
+from models.model_test import DeepLabV3Plus
+from models import sgd_nan
+from configs import default as C
+from dataset.dataloader import Dataloader
+from utils import arg_parser, seed_utils, logger
+from utils import trainer as T
+from utils.early_stopping import EarlyStopping
 
-from configs import default
-from utils import (
-    arg_parser, seed_utils, trainer
-)
+from models.loss import balanced_entropy, class_balanced_loss
 
-if __name__=='__main__':
-    # initial step
-    C = default
-    seed_utils.seed_everything(C.SEED)
+gc.collect()
+warnings.filterwarnings("ignore")
 
-    # parse argument
+torch.backends.cudnn.enabled = False
+
+
+def main():
+    # 초기 설정
+    
+    # 인자 파싱
     args = arg_parser.parse_args(C)
-    arg_parser.print_args(args)
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
     
-    print(type(args))
-    print(args.criterion)
-    # torch.cuda.set_device(args.device_id[0])
-    
-    early_cnt = 0
-    for epoch in range(1, args.epochs):
+    seed_utils.seed_everything(args.seed)
+    # 로거 설정
+    log = logger.get_logger(args.log_dir)
+    for arg in vars(args):
+        s = arg + ": " + str(getattr(args, arg))
+        logging.info(s)
+
+    model = DeepLabV3Plus()
+    torch.cuda.set_device(args.devices_id[0])
+    model = nn.DataParallel(model, device_ids=args.devices_id).cuda()
+    optimizer = sgd_nan.SGD_NanHandler(
+        model.parameters(),
+        lr=args.base_lr,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay,
+        nesterov=True,
+    )
+
+    train_loader, valid_loader = Dataloader(args)
+
+    # EarlyStopping 설정
+    early_stopping = EarlyStopping(
+        patience=args.early_stop,
+        verbose=True,
+        checkpoint_path='./checkpoint.pt',
+        logger=log,
+    )
+
+    for epoch in range(1, args.epochs + 1):
         scaler = GradScaler()
-        
-        # adjust learning rate
-        lr = adjust_learning_rate()
-    
-    
+        msg = T.train_one_epoch(args, train_loader, model, optimizer, epoch, C.LR, scaler)
+        logging.info(msg)
+
+
+if __name__ == '__main__':
+    main()
