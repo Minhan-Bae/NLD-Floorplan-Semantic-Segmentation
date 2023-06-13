e541dfa6fc0b16fc3eb18f49525f6e9f1c045cce
Minhan-Bae <yuyuqw92@gmail.com>
2023-06-13 16:09:28 +0900

    fix: fix issue of dataset, dataloader

--- src/dataset/dataloader.py
+++ src/dataset/dataloader.py
@@ -1,14 +1,33 @@
+import torch
+from torch.utils import data
+
 from .augmentations import get_augmentation
 from .datasets import FloorplanDataset
-from torch.utils import data
-from .augmentations import get_augmentation
-import sys
-sys.path.append('..')
+
+def collate_fn(batch):
+    # 데이터 형식 유효성 검사
+    lengths = [len(s) for s in batch]
+    if not all(length == lengths[0] for length in lengths):
+        raise ValueError("All samples should have the same length.")
+
+    # 배치로 묶인 데이터 생성
+    transposed_batch = list(zip(*batch))  # 튜플로 묶인 배치
+    data = torch.stack(transposed_batch[0], dim=0)  # 데이터 특성
+
+    # 레이블 처리
+    labels = transposed_batch[1][0]  # 리스트의 첫 번째 요소 추출
+    labels = torch.tensor(labels, dtype=torch.float32)  # 레이블을 텐서로 변환
+
+    # 특성별로 묶인 튜플 형태로 데이터 반환
+    return (data, labels)
+
+
+
 def Dataloader(args):
     dataset_train = FloorplanDataset(
         args,
         type = 'train',
-        transform=get_augmentation(data_type="valid")
+        transform=get_augmentation(data_type="train")
     )
     dataset_valid = FloorplanDataset(
         args,
@@ -17,10 +36,10 @@ def Dataloader(args):
     )
     
     train_loader = data.DataLoader(
-        dataset_train, batch_size=args.batch_size, shuffle=True, num_workers=args.workers, drop_last=True
+        dataset_train, batch_size=args.batch_size, shuffle=True, num_workers=args.workers, drop_last=True, collate_fn=collate_fn
     )
     valid_loader = data.DataLoader(
-        dataset_valid, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, drop_last=True
+        dataset_valid, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, drop_last=True, collate_fn=collate_fn
     )
     
     return train_loader, valid_loader
--- src/dataset/datasets.py
+++ src/dataset/datasets.py
@@ -1,5 +1,7 @@
 import os
+import cv2
 import json
+import torch
 import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
@@ -7,7 +9,7 @@ from torchvision.transforms.functional import resize as resize_function
 
 class FloorplanDataset(Dataset):
     def __init__(self, args, type='train', transform=None):
-        self.img_dir = args.img_dir
+        self.image_dir = args.image_dir
         self.img_size = args.image_size
         self.type = type
         self.offset = args.image_offset
@@ -17,25 +19,29 @@ class FloorplanDataset(Dataset):
 
     def __getitem__(self, idx):
         img_name = list(self.data.keys())[idx]
-        img_path = os.path.join(self.img_dir, img_name + '.png')
+        img_path = os.path.join(self.image_dir, img_name + '.png')
         img = Image.open(img_path)
         orig_size = np.array(img.size)  # 원본 이미지의 크기를 저장합니다.
 
         annotation = self.data[img_name]
         min_x = min_y = np.inf
         max_x = max_y = -np.inf
-        for key, values in annotation.items():
+        
+        mask = np.zeros(orig_size, dtype=np.uint8)
+        for idx, (key, values) in enumerate(annotation.items()):
             instance_coords = []  
             for value in values:
-                coords = [float(coord) for coord in value.split(',')]
+                coords = [int(coord) for coord in value.split(',')]
                 coords = np.array(coords).reshape(-1, 2)
                 min_x = min(min_x, coords[:, 0].min())
                 min_y = min(min_y, coords[:, 1].min())
                 max_x = max(max_x, coords[:, 0].max())
                 max_y = max(max_y, coords[:, 1].max())
                 instance_coords.append(coords)
+                cv2.drawContours(mask, [coords], -1, idx+1, -1)
             annotation[key] = instance_coords
-
+        mask = Image.fromarray(mask)
+        
         # offset을 추가하여 crop 범위를 계산합니다.
         min_x = max(0, min_x - self.offset)
         min_y = max(0, min_y - self.offset)
@@ -44,21 +50,27 @@ class FloorplanDataset(Dataset):
 
         # 이미지를 crop합니다.
         img = img.crop((min_x, min_y, max_x, max_y))
-
+        mask = mask.crop((min_x, min_y, max_x, max_y))
         # 좌표도 동일하게 조정합니다.
-        for key, instance_coords in annotation.items():
-            adjusted_coords = []  
-            for coords in instance_coords:
-                coords -= np.array([min_x, min_y])  # crop에 따른 좌표 조정
-                coords = (coords / np.array([max_x - min_x, max_y - min_y])) * np.array(self.img_size)  # Resize에 따른 좌표 조정
-                adjusted_coords.append(coords)
-            annotation[key] = adjusted_coords
+        # for key, instance_coords in annotation.items():
+        #     adjusted_coords = []  
+        #     for coords in instance_coords:
+        #         coords -= np.array([min_x, min_y])  # crop에 따른 좌표 조정
+        #         coords = (coords / np.array([max_x - min_x, max_y - min_y])) * np.array(self.img_size)  # Resize에 따른 좌표 조정
+        #         adjusted_coords.append(coords)
+        #     annotation[key] = adjusted_coords
 
         img = resize_function(img, (self.img_size[1], self.img_size[0]))  # 이미지 크기를 조정합니다.
+        label = resize_function(mask, (self.img_size[1], self.img_size[0]))  # 이미지 크기를 조정합니다.
+        image, label = np.array(img), np.array(label)
         if self.transform:
-            img = self.transform(img)
+            transformed = self.transform(image=image, mask=label)
+            image = transformed['image']
+            label = transformed['mask']
+        image = torch.tensor(image, dtype=torch.float)
+        label = torch.tensor(label, dtype=torch.float)
 
-        return img, annotation
+        return image, label
 
     def __len__(self):
         return len(self.data)
