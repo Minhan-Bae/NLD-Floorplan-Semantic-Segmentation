729bf43e6f3d4e1c502919a2502c695b126103be
Minhan-Bae <yuyuqw92@gmail.com>
2023-07-05 14:17:36 +0900

    feat: update visualization task

--- src/utils/visualize_instance_segmentation.py
+++ src/utils/visualize_instance_segmentation.py
@@ -2,48 +2,50 @@
 # It uses matplotlib to display the images in a grid and to overlay the instance masks.
 # The function is useful for analyzing the performance of instance segmentation models.
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 
-def visualize_instance_segmentation(images_list, masks_list, size=6, shape=(10, 10), title=None, save=None):
-    """
-    Visualize a batch of images with their corresponding instance segmentation masks.
-    
-    Args:
-        images_list (list): A list of images to display.
-        masks_list (list): A list of instance segmentation masks for each image.
-        size (int, optional): Size of the entire grid figure. Default is 6.
-        shape (tuple, optional): Shape of the grid (rows, columns). Default is (10, 10).
-        title (str, optional): Title of the grid. Default is None.
-        save (str, optional): Path to save the grid figure. Default is None.
-    """
-    # Create a figure with a specified size.
-    fig = plt.figure(figsize=(size, size))
-    grid = ImageGrid(fig, 111, nrows_ncols=shape, axes_pad=0.08)
+def encode_with_cutoff(arr, value, cutoff_value):
 
-    # Iterate through images and masks.
-    for ax, image, mask in zip(grid, images_list, masks_list):
-        # Normalize the image to the range [0, 1].
-        image = (image - image.min()) / (image.max() - image.min())
-        mask = (mask - mask.min()) / (mask.max() - mask.min())
-        # image = np.transpose(image, (1, 2, 0))
-        # Overlay the mask on the image.
-        # overlay = np.where(mask > 0, mask, np.nan)
+    encoded_arr = np.zeros_like(arr)  # 모든 요소를 0으로 초기화한 배열 생성
+
+    # 일정 값 이후의 인덱스에 대해 one-hot encoding 수행
+    indices = np.where(arr >= cutoff_value)
+    encoded_arr[indices] = value
+
+    return encoded_arr
+
+def visualize_instance_segmentation(images_list, masks_list, title=None, save=None):
+    ratio = (0.7, 0.3)
+    fig = plt.figure(figsize=(16,16))
+    grid = ImageGrid(fig, 111, nrows_ncols=(4,4), axes_pad=0.08)
+
+    for ax, feature, predict in zip(grid, images_list[:16], masks_list[:16]):
+        feature = feature.detach().cpu().numpy()
+        predict = predict.detach().cpu().numpy()
+
+        feature = np.transpose(feature, (1,2,0))   
+        feature = cv2.cvtColor(feature, cv2.COLOR_BGR2GRAY) # (1, 128, 128)
         
-        # Display the image and overlay the mask.
+        oneHot = np.zeros((256,256), dtype=np.uint8)
+        for idx in range(6):
+            predict[idx] = (predict[idx] - predict[idx].min()) / (predict[idx].max() - predict[idx].min()) * 255
+            p = encode_with_cutoff(predict[idx], (idx/6)*255, 127)
+            p = p.astype(np.uint8)
+            oneHot |= p
         
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-        
-        ax.imshow(image, cmap='gray')
-        ax.imshow(mask, cmap='jet', alpha=0.5)
-        ax.axis('off')
+        ax.imshow(feature, cmap='gray', alpha=ratio[0])
+        ax.imshow(oneHot, cmap='jet', alpha=ratio[1])
+        ax.axis('off')                    
+        if save:
+            plt.savefig(save)
 
     # Display the title if specified.
     if title:
         print(title)
     # Save the figure if specified.
-    if save:
-        plt.savefig(save)
+
     # Show the plot.
     plt.show()
+    plt.close()
